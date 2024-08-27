d12d4568e3196d55e53fa25434af665f823c81b8
Kuro <135591555+An-Ste@users.noreply.github.com>
2024-08-27 14:40:09 +0900

    タイマーアプリを作成しています...
    ポモドーロになれなかった...

--- timer.py
+++ timer.py
@@ -2,35 +2,35 @@ import streamlit as st
 import time
 
 # タイマー部分
-class TimeCounts:
-    # 分
-    def min_time(self, minutes):
-        self.minutes = minutes
-        with st.empty():
-            for m in range(minutes):
-                st.write(f"⏳ {m} seconds have passed")
-                time.sleep(1)
-    # 秒
-    def sec_time(self, seconds):
-        self.seconds = seconds
-        with st.empty():
-            for s in range(seconds):
-                st.write(f"⏳ {s:02} seconds have passed")
-                time.sleep(1)
+# class TimeCounts:
+#     # 分
+#     def min_time(self, minutes):
+#         self.minutes = minutes
+#         with st.empty():
+#             for m in range(minutes):
+#                 st.write(f"⏳ {m} seconds have passed")
+#                 time.sleep(1)
+#     # 秒
+#     def sec_time(self, seconds):
+#         self.seconds = seconds
+#         with st.empty():
+#             for s in range(seconds):
+#                 st.write(f"⏳ {s:02} seconds have passed")
+#                 time.sleep(1)
 
-    def print_time(self, p_hours, p_minutes, p_seconds):
-        p_hours = self.hours
+#     def print_time(self, p_hours, p_minutes, p_seconds):
+#         p_hours = self.hours
 
-        for _ in range(minutes):
-            st.write("{hours:02}：{minutes:02}：{seconds:02}")
+#         for _ in range(minutes):
+#             st.write("{hours:02}：{minutes:02}：{seconds:02}")
 
-def start_timer(self, start_num):
-    self.start_num = start_num
-    st.write(f"{start_num + 1}回目 スタート")
+# def start_timer(self, start_num):
+#     self.start_num = start_num
+#     st.write(f"{start_num + 1}回目 スタート")
 
-def end_timer(self, end_num):
-    self.end_num = end_num
-    st.write(f"{end_num + 1}回目 スタート")
+# def end_timer(self, end_num):
+#     self.end_num = end_num
+#     st.write(f"{end_num + 1}回目 スタート")
 
 # ---------------------------------------------------------------------
 
@@ -39,7 +39,7 @@ min_counts = 0
 min_limits = 0
 hour_counts = 0
 
-st.header("ポモドーロタイマー")
+st.header("タイマー")
 with st.sidebar: # サイドバー
     st.subheader("タイマー 管理メニュー")
     loop_count = st.number_input("タイマーの繰り返し回数", min_value=1, max_value=5, value=1)
