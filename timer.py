4cc04b9382cc79aee0a46530eec27dfff88ce5dc
Kuro <135591555+An-Ste@users.noreply.github.com>
2024-08-29 18:17:00 +0900

    タイマー作成中だヨ！😂
    タイマー頑張って作ってます！応援してネ！

--- timer.py
+++ timer.py
@@ -1,73 +1,50 @@
 import streamlit as st
 import time
 
-# タイマー部分
-# class TimeCounts:
-#     # 分
-#     def min_time(self, minutes):
-#         self.minutes = minutes
-#         with st.empty():
-#             for m in range(minutes):
-#                 st.write(f"⏳ {m} seconds have passed")
-#                 time.sleep(1)
-#     # 秒
-#     def sec_time(self, seconds):
-#         self.seconds = seconds
-#         with st.empty():
-#             for s in range(seconds):
-#                 st.write(f"⏳ {s:02} seconds have passed")
-#                 time.sleep(1)
+# コード本文
+st.title("タイマー")
 
-#     def print_time(self, p_hours, p_minutes, p_seconds):
-#         p_hours = self.hours
-
-#         for _ in range(minutes):
-#             st.write("{hours:02}：{minutes:02}：{seconds:02}")
-
-# def start_timer(self, start_num):
-#     self.start_num = start_num
-#     st.write(f"{start_num + 1}回目 スタート")
-
-# def end_timer(self, end_num):
-#     self.end_num = end_num
-#     st.write(f"{end_num + 1}回目 スタート")
-
-# ---------------------------------------------------------------------
-
-Time = TimeCounts()
-min_counts = 0
-min_limits = 0
-hour_counts = 0
-
-st.header("タイマー")
 with st.sidebar: # サイドバー
     st.subheader("タイマー 管理メニュー")
-    loop_count = st.number_input("タイマーの繰り返し回数", min_value=1, max_value=5, value=1)
-    act_mins = st.number_input("作業時間（分）", min_value=1, max_value=60, value=1)
-    lest_mins = st.number_input("休憩時間（分）", min_value=1, max_value=10, value=1)
+    minutes = st.slider("### **時間を選択してください  (分)**", min_value=0, max_value=10, value=0)
+    seconds = st.slider("### **時間を選択してください  (秒)**", min_value=1, max_value=59, value=1)
+
+min_time = minutes
+sec_time = seconds
+
+min_count = 0
+sec_count = 0
 
 OverWrite = st.empty()
 # タイマー
 col1, col2 = st.columns((1, 1))
-with col1:
+with col1: # スタートボタン
     if st.button("Start", type="primary"):
-        for j in range(loop_count): # ループ回数
-            for i in range(act_mins): # 何分繰り返すか
-                # 秒
+        if minutes == 0: # 0分の場合
+            for secs in range(seconds): # 秒タイマー
                 with st.empty():
-                    for s in range(60):
-                        time.sleep(1)
-                        min_limits += 1
-                        # 分
-                        if min_limits == 61:
-                            min_counts += 1
+                    with OverWrite.container():
+                            sec_count += 1
+                            st.subheader(f"{min_count:02}：{sec_count:02}")
+                            time.sleep(1)
+            with st.empty():
+                with OverWrite.container():
+                    st.subheader(f"⌛ Timer Succeeded !!")
 
-                        with OverWrite.container():
-                            st.subheader(f"{min_counts:02}：{s:02}")
-                            st.subheader(f"{j+1}回目")
-
-                    min_limits = 0
-
-with col2:
+        elif minutes != 0: # 1分以上の場合
+            for mins in range(minutes):
+                if sec_count >= 59: #分タイマー
+                    min_count += 1
+                    for secs in range(seconds): # 秒タイマー
+                        with st.empty():
+                            with OverWrite.container():
+                                    sec_count += 1
+                                    st.subheader(f"{min_count:02}：{sec_count:02}")
+                                    time.sleep(1)
+            with st.empty():
+                with OverWrite.container():
+                    st.subheader(f"⌛ Timer Succeeded !!")
+                    
+with col2: # ストップボタン
     if st.button("Stop", type="secondary"):
         st.write("STOP !!")
